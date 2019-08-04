7988236dabc893749c2d05e8961331c4b354070f
bhchen81 <bhchen81@gmail.com>
2019-08-04 11:12:29 +0800

    part1-unicafe (exercises 1.6 to 1.11) completed

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+
+const Statistic = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+);
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = (1 * good + 0 * neutral + -1 * bad) / total;
+  const positive = `${(good / total) * 100}%`;
+
+  if (total === 0) return <p>No feedback given</p>;
+  else
+    return (
+      <table>
+        <tbody>
+          <Statistic text="good" value={good} />
+          <Statistic text="neutral" value={neutral} />
+          <Statistic text="bad" value={bad} />
+          <Statistic text="all" value={total} />
+          <Statistic text="average" value={average} />
+          <Statistic text="positive" value={positive} />
+        </tbody>
+      </table>
+    );
+};
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={() => setGood(good + 1)} text="good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="bad" />
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
